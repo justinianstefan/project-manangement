9a9c5b0e93a65cfd10e74dd2dca70420baee8acc
Justin <justin.popa20@gmail.com>
2018-04-03 00:03:18 +0300

    Drawer logo

--- src/app/layout/components/sidenav.jsx
+++ src/app/layout/components/sidenav.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 
+import logo from '../../../assets/img/logo.svg'; 
+
 class Sidenav extends Component {
   state = {
     open: true
@@ -9,6 +11,7 @@ class Sidenav extends Component {
   render() {
     return (
       <Drawer open={this.state.open} className="sidenav">
+        <img src={logo} className="App-logo" alt="logo" /> 
         <MenuItem>Menu Item</MenuItem>
         <MenuItem>Menu Item 2</MenuItem>
       </Drawer>
