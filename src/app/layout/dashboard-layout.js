5d63896cd63f49e6c384ee203733da365f739d2c
Octavian Balbinat <octavianbalbinat@gmail.com>
2018-04-03 14:47:49 +0300

    DashboardLayout Test

--- src/app/layout/dashboard-layout.js
+++ src/app/layout/dashboard-layout.js
@@ -1,13 +1,81 @@
 import React, { Component } from 'react';
-import Sidenav from './components/sidenav';
-import Main from './components/main';
+import Projects from '.././projects/projects';
+import Users from '.././users/users';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import Paper from 'material-ui/Paper';
+
+
+
+const paperStyle = {
+  height: '85%',
+  width: "85%",
+  margin: '7%',
+  textAlign: 'center',
+  display: 'inline-block',
+};
+
 
 class DashboardLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "open": false,
+      "show": null
+    };
+  }
+
+  handleToggle = () => this.setState({ open: !this.state.open });
+
+  showProjects = () => {
+    this.setState({ show: 'projects', open: false });
+  };
+
+  showUsers = () => {
+    this.setState({ show: 'users', open: false });
+  };
+
   render() {
+    let content = null;
+
+    switch (this.state.show) {
+      case 'users':
+        content = (<Users />);
+        break;
+
+      case 'projects':
+        content = (<Projects />);
+        break;
+
+      default:
+        content = <h1>Waiting</h1>
+    }
+
     return (
       <div className="dashboard">
-        <Sidenav />
-        <Main />
+
+        <AppBar
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          title="Learn about Drawer, MenuItem and Paper"
+          onLeftIconButtonClick={this.handleToggle}
+        />
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({ open })}>
+
+          <AppBar title="AppBar" />
+          <MenuItem id="showUsersId" onClick={this.showUsers}>Show Users</MenuItem>
+          <MenuItem id="showProjectsId" onClick={this.showProjects}>Show Projects</MenuItem>
+
+        </Drawer>
+        <Paper style={paperStyle} zDepth={9}>
+
+          {content}
+
+        </Paper>
       </div>
     );
   }
---
+++ src/app/projects/projects.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+
+    render() {
+        return (
+            <div>Project IS WOrking!</div>
+        )
+    }
+}
+
+export default Projects;
